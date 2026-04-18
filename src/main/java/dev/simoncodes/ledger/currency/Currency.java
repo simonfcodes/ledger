1230a79982b0beba0968c2c31729ce8e8ade9125
Simon F <simon@simoncodes.dev>
2026-04-18 23:50:07 +0100

    feat: Annotate Currency record with Table mapping for database integration

--- src/main/java/dev/simoncodes/ledger/currency/Currency.java
+++ src/main/java/dev/simoncodes/ledger/currency/Currency.java
@@ -1,7 +1,9 @@
 package dev.simoncodes.ledger.currency;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
+@Table("currencies")
 public record Currency (
         @Id String code,
         String name,
